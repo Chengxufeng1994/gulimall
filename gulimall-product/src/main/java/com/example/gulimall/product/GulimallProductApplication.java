55629641d801149570069c0ca1ca4d2bc5b35729
benny cheng <benny12628@gmail.com>
2022-06-24 22:48:02 +0800

    18 - 基本 CRUD 功能

--- gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,27 @@
 package com.example.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1. 整合 MyBatis-Plus
+ *  1. 導入依賴
+ *          <dependency>
+ *             <groupId>com.baomidou</groupId>
+ *             <artifactId>mybatis-plus-boot-starter</artifactId>
+ *             <version>3.2.0</version>
+ *         </dependency>
+ *  2. 配置
+ *      1. 配置數據源
+ *          1. 導入數據庫驅動
+ *          2. application.yml 配置數據源相關訊息
+ *      2. 配置 MyBatis-Plus
+ *          1. 使用 @MapperScan
+ *          2. 告訴 MyBatisPlus, sql 映射文件
+ */
+
+@MapperScan("com.example.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
