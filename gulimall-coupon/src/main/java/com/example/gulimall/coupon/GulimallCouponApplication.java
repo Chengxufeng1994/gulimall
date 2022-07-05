21f975e6693f608c7a96f1e5472f3e73bbf52599
benny cheng <benny12628@gmail.com>
2022-07-05 23:27:20 +0800

    24 - SpringCloud Alibaba Nacos 命名空間

--- gulimall-coupon/src/main/java/com/example/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/example/gulimall/coupon/GulimallCouponApplication.java
@@ -4,6 +4,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 1. 如何使用 Nacos 作為配置中心統一管理配置
+ *
+ *  1. 引入依賴
+ *         <dependency>
+ *             <groupId>com.alibaba.cloud</groupId>
+ *             <artifactId>spring-cloud-starter-alibaba-nacos-config</artifactId>
+ *         </dependency>
+ *
+ *         <dependency>
+ *             <groupId>org.springframework.cloud</groupId>
+ *             <artifactId>spring-cloud-starter-bootstrap</artifactId>
+ *         </dependency>
+ *   2. 創建 bootstrap.properties
+ *      spring.application.name=gulimall-coupon
+ *      spring.cloud.nacos.config.server-addr=192.168.56.10:8848
+ *   3. 需要給配置中心默認添加一個叫 數據集 (Data Id) gulimall-coupon.properties 默認規則. 應用名.properties
+ *   4. 給應用名.properties 添加任何配置
+ *   5. 動態獲取配置
+ *      @RefresScope
+ *      @Valur
+ *  2. 細節
+ *      1. 命名空間, 配置隔離
+ *      2. 配置集
+ *      3. 配置集 ID
+ *      4. 配置分組
+ */
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
